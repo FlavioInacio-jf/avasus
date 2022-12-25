987d29f0c39bd47af6770364d94e3d6fff26a6db
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-12-25 07:49:13 -0300

    refactor: Criado o header principal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,68 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
 export const Header: FC = () => {
   return (
     <header className='w-full'>
       <div className='max-w-[1700px] mx-auto py-[1.6rem] max-h-[7rem] flex items-center justify-between'>
-        <Link
-          href='/'
-          prefetch={false}
-          className='w-[180px] h-[30px] relative block'
-        >
-          <Image src='/brands/brand-ava-sus-mixed-color.svg' alt='' fill />
+        <Link href='/' prefetch={false} className='relative block'>
+          <Image
+            src='/brands/brand-ava-sus-mixed-color.svg'
+            alt='Logo Avasus'
+            width={200}
+            height={40}
+          />
         </Link>
-        <nav className='font-semibold text-xl'>
-          <Link href=''>Início</Link>
-          <Link href=''>Sobre Nós</Link>
-          <Link href=''>Cursos</Link>
-          <Link href=''>Parceiros</Link>
-          <Link href=''>Transparência</Link>
-          <Link href=''>Contato</Link>
+        <nav className='font-semibold text-lg flex-1 ml-16'>
+          <Link href='' className='mr-8'>
+            Início
+          </Link>
+          <Link href='' className='mr-8'>
+            Sobre Nós
+          </Link>
+          <Link href='' className='mr-8'>
+            Cursos
+          </Link>
+          <Link href='' className='mr-8'>
+            Parceiros
+          </Link>
+          <Link href='' className='mr-8'>
+            Transparência
+          </Link>
+          <Link href='' className='mr-8'>
+            Contato
+          </Link>
         </nav>
-        <input type='search' placeholder='Busca por um assunto...' />
-        <Link href='/' prefetch={false}>
-          Entrar
-        </Link>
-        <Link href='/' prefetch={false}>
-          Cadastrar
-        </Link>
+
+        <div className='flex'>
+          <div className='flex-1'>
+            <label className='sr-only'>Pesquisar</label>
+            <div className='relative flex items-center border-2 border-black/20 rounded-full px-5 py-2'>
+              <BsSearch className='text-xl text-[#545454] mr-2' />
+              <input
+                type='search'
+                placeholder='Busca por um assunto...'
+                className=' placeholder:text-[#545454] outline-none'
+              />
+            </div>
+          </div>
+          <Link
+            href='/'
+            prefetch={false}
+            className='btn btn-lg btn-secondary-outline ml-4'
+          >
+            Entrar
+          </Link>
+          <Link
+            href='/'
+            prefetch={false}
+            className='btn btn-lg btn-secondary ml-4'
+          >
+            Cadastrar
+          </Link>
+        </div>
       </div>
     </header>
   );
