faece10ab0d31fc754d12dc2b0063ed24cecf823
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-12-28 19:24:00 -0300

    refactor: Adição da seção parceiros na página inicial

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import { BannerHome } from './home/BannerHome';
+import { contentsPartnersSection } from './home/contents-partners-section';
 import { EducacionalModule } from './home/EducationalModule';
 
 const Page: NextPage = () => {
@@ -7,6 +8,27 @@ const Page: NextPage = () => {
     <main className='w-full'>
       <BannerHome />
       <EducacionalModule />
+      <section className='w-full px-4 mt-24 mb-32 xss:mb-16 xs:mb-16 sm:mb-16 md:mb-16'>
+        <div className='w-full max-w-[1110px] mx-auto'>
+          <h2 className='title title-3xl title-primary text-center mb-8'>
+            Parceiros
+          </h2>
+          <div className='w-full bg-[#F5F5F7] py-7 px-8 rounded-[20px]'>
+            <ul className='flex gap-8 xss:flex-col xs:flex-col sm:flex-col md:flex-col'>
+              {contentsPartnersSection.map(({ title, detail }) => (
+                <li key={title} className='flex-1'>
+                  <h3 className='title title-2xl title-black mb-[1.125rem] leading-none text-center'>
+                    {title}
+                  </h3>
+                  <p className='font-semibold xss:text-base xs:text-base text-lg text-[#707070] text-center leading-tight'>
+                    {detail}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
