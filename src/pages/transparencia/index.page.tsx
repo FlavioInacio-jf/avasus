f4738fe0fa9785a5e26dcbe0ef527e12c24f635f
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2023-01-08 22:45:05 -0300

    refactor: Add gráficos na pagina de transparência

--- src/pages/transparencia/index.page.tsx
+++ src/pages/transparencia/index.page.tsx
@@ -1,16 +1,32 @@
+import {
+  ArcElement,
+  Chart as ChartJS,
+  ChartData,
+  Legend,
+  Tooltip,
+} from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { GetServerSideProps, GetServerSidePropsResult, NextPage } from 'next';
 import Image from 'next/image';
+import { Pie } from 'react-chartjs-2';
+import { Chart } from 'react-google-charts';
 import { displayQuantity } from '../../helpers';
 import { ITransparency } from '../../interfaces';
 import { transparencyService } from '../../services';
+
 import { Box } from './Box';
 import { Head } from './head';
 
 interface PageProps {
   transparency: ITransparency;
 }
+const colorsheme = ['#FFFFFF', '#D2202C', '#707070', '#2F2E41'];
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const Page: NextPage<PageProps> = ({ transparency }) => {
-  const { dados_gerais } = transparency;
+  const { dados_gerais, usuarios_por_curso, usuarios_por_estado } =
+    transparency;
   const {
     usuarios_registrados,
     incricoes_realizadas,
@@ -19,6 +35,25 @@ const Page: NextPage<PageProps> = ({ transparency }) => {
     investimento_medio_aluno,
     investimento_medio_curso,
   } = dados_gerais;
+
+  const pieChartOptions: ChartData<'pie', number[], string> = {
+    labels: usuarios_por_curso.map(({ curso }) => curso),
+    datasets: [
+      {
+        label: '# Usuários por curso',
+        data: usuarios_por_curso.map(({ usuarios }) => usuarios),
+        backgroundColor: colorsheme,
+        borderWidth: 1,
+      },
+    ],
+  };
+  const geoChartData = usuarios_por_estado.map(
+    ({ estados, direito_certificacao, usuarios_totais }) => [
+      `BR-${estados}`,
+      direito_certificacao,
+      usuarios_totais,
+    ]
+  );
   return (
     <>
       <Head />
@@ -96,7 +131,7 @@ const Page: NextPage<PageProps> = ({ transparency }) => {
                           {displayQuantity(direito_certificacao)}
                         </span>
                       </li>
-                      <li className='md:col-span-2 w-full flex xss:grid xs:grid sm:grid md:grid xss:grid-cols-1 xs:grid-cols-1 sm:grid-cols-1  md:grid-cols-2 flex-wrap gap-y-[42px] justify-between'>
+                      <li className='md:col-span-2 w-full flex xss:grid xs:grid sm:grid md:grid xss:grid-cols-1 xs:grid-cols-1 sm:grid-cols-1  md:grid-cols-2 flex-wrap gap-y-[42px] justify-center'>
                         <div className='flex flex-col items-center mr-[39px]'>
                           <div className='flex items-center font-bold'>
                             <Image
@@ -132,17 +167,106 @@ const Page: NextPage<PageProps> = ({ transparency }) => {
                   </div>
                 </Box>
               </div>
-              <div className='col-span-1 h-[667px]'>
+              <div className='col-span-1 h-[667px] xss:h-full xs:h-full sm:h-full md:h-full'>
                 <Box>
                   <h3 className='title title-primary title-2xl text-center'>
                     Usuários por curso
                   </h3>
+                  <div className='w-fit h-fit mt-4 mx-auto'>
+                    <Pie
+                      data={pieChartOptions}
+                      className='w-[500px] xss:w-full xs:w-full sm:w-full md:w-full mx-auto'
+                      plugins={[ChartDataLabels]}
+                      options={{
+                        responsive: true,
+                        plugins: {
+                          datalabels: {
+                            offset: 150,
+                            display: false,
+                          },
+                          tooltip: {
+                            enabled: true,
+                          },
+                          legend: {
+                            display: true,
+                            position: 'bottom',
+                            textDirection: 'left',
+                            fullSize: true,
+                            align: 'start',
+                            title: { position: 'end' },
+                            labels: {
+                              font: { weight: '700', size: 16 },
+                              padding: 15,
+                              color: '#2F2E41',
+                              textAlign: 'left',
+                              pointStyle: 'circle',
+                              usePointStyle: true,
+                              boxHeight: 20,
+                              boxWidth: 20,
+                              filter: (legendItem, data) => {
+                                const label = legendItem.text;
+                                const labelIndex = data.labels?.findIndex(
+                                  (labelName) => labelName === label
+                                );
+                                const qtd =
+                                  data.datasets[0].data[labelIndex as number];
+
+                                legendItem.text = `${
+                                  legendItem.text
+                                }: ${displayQuantity(qtd as number)}`;
+
+                                return true;
+                              },
+                            },
+                          },
+                        },
+                      }}
+                    />
+                  </div>
                 </Box>
               </div>
               <div className='col-span-1 h-[667px]'>
                 <Box>
-                  <h3 className='title title-primary title-2xl text-center'>
+                  <h3 className='title title-primary title-2xl text-center '>
                     Usuários por Estado
+                    <div className='w-fit h-fit mt-4 mx-auto flex items-center'>
+                      <Chart
+                        chartType='GeoChart'
+                        data={[
+                          [
+                            'State',
+                            'Direito a certificacão',
+                            'Usuários totais',
+                          ],
+                          ...geoChartData,
+                        ]}
+                        options={{
+                          responsive: true,
+                          backgroundColor: 'transparent',
+                          region: 'BR',
+                          displayMode: 'markers',
+                          colorAxis: {
+                            colors: colorsheme,
+                          },
+                          sizeAxis: {
+                            minSize: 10,
+                            maxSize: 15,
+                          },
+                          resolution: 'provinces',
+                          datalessRegionColor: '#E8E8E8',
+                          defaultColor: '#f5f5f5',
+                          zoomFactor: 0,
+                          legend: true,
+                          forceIFrame: true,
+                          enableRegionInteractivity: true,
+                          tooltip: {
+                            isHtml: true,
+                          },
+                        }}
+                        mapsApiKey='AIzaSyB2xcE2fyWJXfOOHxkMglQvBwfqdJtpCuE'
+                        chartLanguage='pt-BR'
+                      />
+                    </div>
                   </h3>
                 </Box>
               </div>
